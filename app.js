e4933d29cf3970c974c8c51b8f21fe7245c2ec7b
Shaahin Shahbazi <shaahin@gmail.com>
2015-01-09 14:23:31 -0600

    productsList as service, feeding storeController

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // wrapping the whole thing with a function is a good practice
 (function(){
 	var store = angular.module('Store', []); // This will be our Angular base
-	// static
-	productsList = [
-					{ name: "Nexus 4", price: 300, img: "product-img/n4.jpg"},
-					{ name: "Nexus 5", price: 350, reviews: ["Too big. Yes I'm a girl. Ha Ha."] },	
-					{ name: "Nexus 6", price: 640},	
-					{ name: "Plus one", price: 350, img: "product-img/oneplus.jpg"},
-					{ name: "Xperia Z3", price: 550, reviews: ["Nice and classy."] },
-				];
-					
+	// service
+	store.factory('reviewSrv', function() {
+		// static
+		productsList = [
+				{ name: "Nexus 4", price: 300, img: "product-img/n4.jpg"},
+				{ name: "Nexus 5", price: 350, reviews: ["Too big. Yes I'm a girl. Ha Ha."] },	
+				{ name: "Nexus 6", price: 640},	
+				{ name: "Plus one", price: 350, img: "product-img/oneplus.jpg"},
+				{ name: "Xperia Z3", price: 550, reviews: ["Nice and classy."] },
+			];
+			
+		return {
+			productsList: productsList
+		}
+						
+	});
+						
 		
-	store.controller('storeController', function() {
-		this.products = productsList;
-	});	
+	store.controller('storeController', ['reviewSrv', function(reviewSrv) {
+		this.products = reviewSrv.productsList;
+	}]);	
 	
 	store.controller('reviewController', function() {
 		this.newReview = ""; // will use ng-model to bind this to the textarea window
